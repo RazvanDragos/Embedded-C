1e3db00b0e1402ff466b069ffc8673966b9c4c72
RazvanDragos <razvan_dragos@yahoo.com>
2019-06-20 21:08:52 +0300

    Created first C++ hello program

---
+++ C++/hello.cpp
@@ -0,0 +1,13 @@
+/*Commands to compile and run:
+    g++ file.cpp
+    g++ -o file file.cpp
+    ./hello
+ */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello!";
+    return 0;
+}
